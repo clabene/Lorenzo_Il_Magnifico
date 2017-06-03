9fae88f4a91679767ef703a0cd9da3d8df150131
Claudio Benedetti <claudio.benedetti@mail.polimi.it>
2017-06-03 14:21:31 +0200

    bug fixed

--- src/excommunicationTessels/MalusOnFamilyMemberPlacementTessel.java
+++ src/excommunicationTessels/MalusOnFamilyMemberPlacementTessel.java
@@ -13,12 +13,14 @@ import player.Player;
  * Created by IBM on 02/06/2017.
  */
 public class MalusOnFamilyMemberPlacementTessel extends ExcommunicationTassel{
-
+    /*
     private PeriodNumber periodNumber;
     private ActionSpaceType actionSpaceType;
     private ActivationActionSpaceType activationActionSpaceType;
     private CardType cardType;
     private int malus;
+    */
+    private BonusOnFamilyMemberPlacement malusToGiveToPlayer;
 
     /*
     * Observation: if action space type is market and malus = Integer.MIN_VALUE, the "no market for you" tassel is implemented too
@@ -26,28 +28,37 @@ public class MalusOnFamilyMemberPlacementTessel extends ExcommunicationTassel{
 
     public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber, CardType cardType, int malus){
         super(periodNumber);
-        this.cardType = cardType;
-        this.malus = malus;
+        //this.cardType = cardType;
+        // this.malus = malus;
+        this.malusToGiveToPlayer = new BonusOnFamilyMemberPlacement(cardType, -1*malus);
+
     }
     public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber, ActivationActionSpaceType activationActionSpaceType /*ActionSpaceType actionSpaceType*/, int malus){
         super(periodNumber);
-        this.activationActionSpaceType = activationActionSpaceType;
-        this.actionSpaceType = ActionSpaceType.ACTIVATION;
+        //this.activationActionSpaceType = activationActionSpaceType;
+        //this.actionSpaceType = ActionSpaceType.ACTIVATION;
         //this.actionSpaceType = actionSpaceType;
-        this.malus = malus;
+        //this.malus = malus;
+        this.malusToGiveToPlayer = new BonusOnFamilyMemberPlacement(activationActionSpaceType, -1*malus);
+
     }
     public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber){
         super(periodNumber);
+        this.malusToGiveToPlayer = new BonusOnFamilyMemberPlacement(-1);
     }
 
 
     @Override
     public void activate(Player player) {
+        /*
         if(actionSpaceType == null && cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(-1));
-        if(actionSpaceType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(cardType, -1*malus));
-        //if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(actionSpaceType, -1*malus));
-        if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(activationActionSpaceType, -1*malus));
 
+        if(actionSpaceType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(cardType, -1*malus));
+
+
+        if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(activationActionSpaceType, -1*malus));
+        */
+        player.getBonuses().add(malusToGiveToPlayer);
 
     }
 
