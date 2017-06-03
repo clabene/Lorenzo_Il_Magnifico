af2342d622de4309e6cceff4b8924a7c897edb32
Claudio Benedetti <claudio.benedetti@mail.polimi.it>
2017-06-03 14:18:03 +0200

    bug fixed

--- src/excommunicationTessels/MalusOnFamilyMemberPlacementTessel.java
+++ src/excommunicationTessels/MalusOnFamilyMemberPlacementTessel.java
@@ -2,6 +2,8 @@ package excommunicationTessels;
 
 
 import actionSpaces.ActionSpaceType;
+import actionSpaces.ActivationActionSpace;
+import actionSpaces.ActivationActionSpaceType;
 import bonuses.BonusOnFamilyMemberPlacement;
 import cards.CardType;
 import gameStructure.PeriodNumber;
@@ -14,6 +16,7 @@ public class MalusOnFamilyMemberPlacementTessel extends ExcommunicationTassel{
 
     private PeriodNumber periodNumber;
     private ActionSpaceType actionSpaceType;
+    private ActivationActionSpaceType activationActionSpaceType;
     private CardType cardType;
     private int malus;
 
@@ -26,9 +29,11 @@ public class MalusOnFamilyMemberPlacementTessel extends ExcommunicationTassel{
         this.cardType = cardType;
         this.malus = malus;
     }
-    public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber, ActionSpaceType actionSpaceType, int malus){
+    public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber, ActivationActionSpaceType activationActionSpaceType /*ActionSpaceType actionSpaceType*/, int malus){
         super(periodNumber);
-        this.actionSpaceType = actionSpaceType;
+        this.activationActionSpaceType = activationActionSpaceType;
+        this.actionSpaceType = ActionSpaceType.ACTIVATION;
+        //this.actionSpaceType = actionSpaceType;
         this.malus = malus;
     }
     public MalusOnFamilyMemberPlacementTessel(PeriodNumber periodNumber){
@@ -40,7 +45,9 @@ public class MalusOnFamilyMemberPlacementTessel extends ExcommunicationTassel{
     public void activate(Player player) {
         if(actionSpaceType == null && cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(-1));
         if(actionSpaceType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(cardType, -1*malus));
-        if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(actionSpaceType, -1*malus));
+        //if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(actionSpaceType, -1*malus));
+        if(cardType == null) player.getBonuses().add(new BonusOnFamilyMemberPlacement(activationActionSpaceType, -1*malus));
+
 
     }
 
