331120897c178ed0218919353c11b64664ef4bc9
Pino Surace <pino.surace@student.tut.fi>
2017-06-06 11:36:21 +0200

    testing

--- src/logic/gameStructure/Game.java
+++ src/logic/gameStructure/Game.java
@@ -31,13 +31,13 @@ public class Game {
         winnerElector.setPlayers(this.players); //game starts when all players joined
 
         Period firstPeriod = new Period(players);
-        firstPeriod.startPeriod(3, tessels[0]);
+        firstPeriod.startPeriod(3, tassels[0]);
         //todo change of turn order also between periods
         Period secondPeriod = new Period(players);
-        secondPeriod.startPeriod(4, tessels[1]);
+        secondPeriod.startPeriod(4, tassels[1]);
 
         Period thirdPeriod = new Period(players);
-        thirdPeriod.startPeriod(5, tessels[2]);
+        thirdPeriod.startPeriod(5, tassels[2]);
 
 
         winnerElector.getWinner();
